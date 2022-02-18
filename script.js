7f1e8dc8b8b7d16cfcaf041c62843a49266520b1
CrimsonMax <61148415+CrimsonMax@users.noreply.github.com>
2022-02-18 20:19:05 +0300

    comment

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const slidesContainer = document.querySelectorAll('.image-container')
 const dotsContainer = document.querySelector('.my-slider-dots-container')
-slidesContainer.forEach(elem => dotsContainer.appendChild(document.createElement('div').classList.add('dot')))
+// slidesContainer.forEach(elem => dotsContainer.appendChild(document.createElement('div').classList.add('dot')))
 
 console.log(slidesContainer)
