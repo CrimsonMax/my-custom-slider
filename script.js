7f9bd6c907719e62c91d9d8e5e1de6e6d6e04271
CrimsonMax <61148415+CrimsonMax@users.noreply.github.com>
2022-02-21 13:29:13 +0300

    set dynamic dots and width

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 const slidesContainer = document.querySelectorAll('.image-container')
 const dotsContainer = document.querySelector('.my-slider-dots-container')
-slidesContainer.forEach(elem => dotsContainer.appendChild(document.createElement('div').classList.add('dot')))
+const primeContainer = document.querySelector('.my-slider-main-container')
+const imageContainer = document.querySelector('.my-slider-image-container')
 
-console.log(slidesContainer)
+let slidesTotal = getComputedStyle(primeContainer).getPropertyValue('--slides')
+let slidesWidth = getComputedStyle(imageContainer).getPropertyValue('width')
+
+slidesTotal *= slidesContainer.length
+
+let properWidth = `${parseInt(slidesWidth) * slidesTotal}px`
+
+imageContainer.style.setProperty('width', properWidth)
+
+slidesContainer.forEach(elem => {
+  let dot = document.createElement('div')
+  dot.classList.add('dot')
+  dot.dataset.dot = elem.firstElementChild.dataset.img
+  dotsContainer.appendChild(dot)
+})
+
+const activateSlide = (e) => {
+  if (e.target.classList.value !== 'dot') return
+
+  slidesContainer.forEach(elem => {
+    elem.classList.remove('active-slide')
+    if (elem.firstElementChild.dataset.img === e.target.dataset.dot) {
+      elem.classList.add('active-slide')
+    }
+  })
+}
+
+dotsContainer.addEventListener('click', activateSlide)
+
+// console.log()
+
+// slidesContainer.forEach(elem => dotsContainer.appendChild(document.createElement('div').classList.add('dot')))
+
+// console.log(slidesContainer)
