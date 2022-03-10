6ba4ccce5e598d06308aa8779dfc0d0abb9da3e5
CrimsonMax <61148415+CrimsonMax@users.noreply.github.com>
2022-03-10 19:33:54 +0300

    1/3 complete

--- script.js
+++ script.js
@@ -1,42 +1,92 @@
+// TODO: Drag, Endless, Action Dot
+
 // Setup prime elements
-const slidesContainer = document.querySelectorAll('.image-container')
+const primeContainer = document.querySelector('.my-slider-main-container') // -- ??
+const sliderContainer = document.querySelector('.my-slider-image-container')
+const mySlides = document.querySelectorAll('.image-container')
 const dotsContainer = document.querySelector('.my-slider-dots-container')
-const primeContainer = document.querySelector('.my-slider-main-container')
-const imageContainer = document.querySelector('.my-slider-image-container')
+
+const sliderLength = mySlides.length
+const slideWidth = mySlides[0].offsetWidth
+
+const containerWidth = primeContainer.offsetWidth
+
+const firstSlide = mySlides[0]
+const lastSlide = mySlides[sliderLength - 1]
+
+const cloneFirstSlide = firstSlide.cloneNode(true)
+const cloneLastSlide = lastSlide.cloneNode(true)
+
+let posX1;
+let posX2;
+let initialPosition;
+let finalPosition;
+
+// Generate dots
+mySlides.forEach(elem => {
+  let dot = document.createElement('div')
+  dot.classList.add('dot')
+  dot.dataset.dot = elem.firstElementChild.dataset.img
+  // if (elem.classList.contains('active-slide')) {
+  //   dot.classList.add('active-dot')
+  // }
+  dotsContainer.appendChild(dot)
+})
+
+// function addActiveDot(nodes) {
+  
+// }
+
+// Set clone slides
+// sliderContainer.appendChild(cloneFirstSlide)
+// sliderContainer.insertBefore(cloneLastSlide, firstSlide)
 
 // Setup buttons
 const leftButton = document.querySelector('.my-slider-arrow-left')
 const rightButton = document.querySelector('.my-slider-arrow-right')
 
-// Generate dots
-slidesContainer.forEach(elem => {
-  let dot = document.createElement('div')
-  dot.classList.add('dot')
-  dot.dataset.dot = elem.firstElementChild.dataset.img
-  dotsContainer.appendChild(dot)
-})
-
-
-
-
-// Setup slider length
-let slidesTotal = slidesContainer.length
 // Setup full slider width
-let properWidth = 100 * slidesTotal
-imageContainer.style.setProperty('width', `${properWidth}%`)
-// Setup one slide width
-let imageStep = 100 / slidesTotal
+let properWidth = containerWidth * sliderLength
+sliderContainer.style.setProperty('width', `${properWidth}px`)
 
-let sliderLast = slidesContainer[slidesContainer.length - 1]
+// console.log(sliderContainer.style.left)
+// console.log(sliderContainer.offsetLeft)
 
+// Set actions
+leftButton.addEventListener('click', leftMove)
+rightButton.addEventListener('click', rightMove)
+dotsContainer.addEventListener('click', activateSlide)
 
-imageContainer.insertAdjacentElement('afterbegin', sliderLast)
+// Drag slides
+// sliderContainer.addEventListener("mousedown", dragStart);
+// sliderContainer.addEventListener("touchstart", dragStart);
+// sliderContainer.addEventListener("touchmove", dragMove);
+// sliderContainer.addEventListener("touchend", dragEnd);
 
+function leftMove() {
+  console.log('Left')
+  // console.log(sliderContainer.style.left / containerWidth)
+  if (sliderContainer.offsetLeft % containerWidth === 0) {
+    sliderContainer.style.left = `${sliderContainer.offsetLeft - containerWidth}px`
+  }
+  console.log(+sliderContainer.style.left)
+  console.log(sliderContainer.offsetLeft)
+  console.log(containerWidth)
+}
 
-const activateSlide = (e) => {
+function rightMove() {
+  console.log('Right')
+  if (sliderContainer.offsetLeft % containerWidth === 0) {
+    sliderContainer.style.left = `${sliderContainer.offsetLeft + containerWidth}px`
+  }
+  console.log(sliderContainer.style.left)
+  console.log(sliderContainer.offsetLeft)
+}
+
+function activateSlide(e) {
   if (e.target.classList.value !== 'dot') return
 
-  slidesContainer.forEach((elem, index) => {
+  mySlides.forEach((elem, index) => {
     elem.firstElementChild.dataset.img === e.target.dataset.dot
       ?
       elem.classList.add('active-slide')
@@ -44,125 +94,168 @@ const activateSlide = (e) => {
       elem.classList.remove('active-slide')
 
     if (elem.classList.contains('active-slide')) {
-      console.log(elem.offsetWidth)
+      console.log(elem.classList[0])
+      // console.log(elem.getClientRects())
+      console.log(index)
+      // console.log(Math.floor(sliderLength / 2) <= index ? 'right' : 'left')
+      sliderContainer.style.left = `${index * (-containerWidth)}px`
     }
   })
 }
 
+////////////////////////////////////////////////////////////////
+
+// // Setup slider length
+// let slidesTotal = mySlides.length
 
 
-let slideCoordinates = 0
+// // Setup one slide width
+// let imageStep = 100 / slidesTotal
 
-// let firstSlider = slidesContainer[0];
+// let sliderLast = mySlides[mySlides.length - 1]
 
-const leftMove = () => {
-  let firstSlider = document.querySelectorAll('.image-container')[0]
 
-  imageContainer.style.marginLeft = '-200%'
-  imageContainer.style.transition = 'all .5s ease'
+// sliderContainer.insertAdjacentElement('afterbegin', sliderLast)
 
-  setTimeout(() => {
-    imageContainer.style.transition = 'none'
-    imageContainer.insertAdjacentElement('beforeend', firstSlider)
-    imageContainer.style.marginLeft = '-100%'
-  }, 500);
+
+// const activateSlide = (e) => {
+//   if (e.target.classList.value !== 'dot') return
+
+//   mySlides.forEach((elem, index) => {
+//     elem.firstElementChild.dataset.img === e.target.dataset.dot
+//       ?
+//       elem.classList.add('active-slide')
+//       :
+//       elem.classList.remove('active-slide')
+
+//     if (elem.classList.contains('active-slide')) {
+//       console.log(elem.offsetWidth)
+//     }
+//   })
+// }
+
+
+
+// let slideCoordinates = 0
+
+//////////////////////////////////////////////////////////////////////
+
+// let firstSlider = mySlides[0];
+
+//////////////////////////////////////////////////////////////////////
+
+// const leftMove = () => {
+//   let firstSlider = document.querySelectorAll('.image-container')[0]
+
+//   sliderContainer.style.marginLeft = '-200%'
+//   sliderContainer.style.transition = 'all .5s ease'
+
+//   setTimeout(() => {
+//     sliderContainer.style.transition = 'none'
+//     sliderContainer.insertAdjacentElement('beforeend', firstSlider)
+//     sliderContainer.style.marginLeft = '-100%'
+//   }, 500);
+
+//   // let step
+//   // debugger
+
+//   // TODO check if First or Last elem in Array
+
+//   // mySlides.forEach((elem, index) => {
+//   //   if (elem.classList.contains('active-slide')) {
+//   //     // console.log(mySlides[index])
+//   //     elem.classList.remove('active-slide')
+//   //     return step = index
+//   //   }
+//   // })
+
+//   // if (mySlides[mySlides.length - 1].classList.contains()) {
+//   //   sliderContainer.style.transform = `translateX(${img - imageStep}%)`
+//   //   mySlides[step].style.order = '-1'
+//   //   sliderContainer.style.transition = 'none'
+//   //   sliderContainer.style.transform = `translateX(${0}%)`
+//   //   return
+//   // }
+
+//   // let img = imageStep * -step
+
+//   // sliderContainer.style.transform = `translateX(${img - imageStep}%)`
+//   // mySlides[++step].classList.add('active-slide')
+
+//   // if (step === mySlides.length - 1) {
+//   //   // let img = imageStep * -step
+
+//   //   // sliderContainer.style.transform = `translateX(${img - imageStep}%)`
+//   //   // setTimeout(() => {
+//   //   // }, 2);
+//   //   sliderContainer.style.transition = 'none'
+//   //   sliderContainer.style.transform = `translateX(${imageStep}%)`
+//   //   // mySlides[step + 1].classList.add('active-slide')
+
+//   //   // mySlides[step].style.transition = '100s'
+//   //   // mySlides[step].style.transform = `translateX(${50}%)`
+//   //   mySlides[step].style.order = '-1'
+//   //   // mySlides[step].style.transform = `translateX(${0}%)`
+//   //   setTimeout(() => {
+//   //     sliderContainer.style.transition = 'all .4s ease'
+//   //     sliderContainer.style.transform = `translateX(${0}%)`
+//   //   }, 4);
+//   //   // sliderContainer.style.transition = 'all .4s ease'
+//   //   // sliderContainer.style.transform = `translateX(${0}%)`
+//   //   return
+//   // }
+
+// }
+
+////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////
+
+// const rightMove = () => {
+
+//   let slider = document.querySelectorAll('.image-container')
+//   let lastSection = slider[slider.length - 1]
+
+//   sliderContainer.style.marginLeft = '0%'
+//   sliderContainer.style.transition = 'all .5s ease'
+
+//   setTimeout(() => {
+//     sliderContainer.style.transition = 'none'
+//     sliderContainer.insertAdjacentElement('afterbegin', lastSection)
+//     sliderContainer.style.marginLeft = '-100%'
+//   }, 500);
+
+  ////////////////////////////////////////////////////////////
 
   // let step
-  // debugger
-
-  // TODO check if First or Last elem in Array
-
-  // slidesContainer.forEach((elem, index) => {
+  // mySlides.forEach((elem, index) => {
   //   if (elem.classList.contains('active-slide')) {
-  //     // console.log(slidesContainer[index])
   //     elem.classList.remove('active-slide')
   //     return step = index
   //   }
   // })
-
-  // if (slidesContainer[slidesContainer.length - 1].classList.contains()) {
-  //   imageContainer.style.transform = `translateX(${img - imageStep}%)`
-  //   slidesContainer[step].style.order = '-1'
-  //   imageContainer.style.transition = 'none'
-  //   imageContainer.style.transform = `translateX(${0}%)`
-  //   return
-  // }
-
   // let img = imageStep * -step
+  // // if (!mySlides[step--]) return console.log('hello')
+  // mySlides[--step].classList.add('active-slide')
+  // sliderContainer.style.transform = `translateX(${img + imageStep}%)`
+// }
 
-  // imageContainer.style.transform = `translateX(${img - imageStep}%)`
-  // slidesContainer[++step].classList.add('active-slide')
 
-  // if (step === slidesContainer.length - 1) {
-  //   // let img = imageStep * -step
-
-  //   // imageContainer.style.transform = `translateX(${img - imageStep}%)`
-  //   // setTimeout(() => {
-  //   // }, 2);
-  //   imageContainer.style.transition = 'none'
-  //   imageContainer.style.transform = `translateX(${imageStep}%)`
-  //   // slidesContainer[step + 1].classList.add('active-slide')
-
-  //   // slidesContainer[step].style.transition = '100s'
-  //   // slidesContainer[step].style.transform = `translateX(${50}%)`
-  //   slidesContainer[step].style.order = '-1'
-  //   // slidesContainer[step].style.transform = `translateX(${0}%)`
-  //   setTimeout(() => {
-  //     imageContainer.style.transition = 'all .4s ease'
-  //     imageContainer.style.transform = `translateX(${0}%)`
-  //   }, 4);
-  //   // imageContainer.style.transition = 'all .4s ease'
-  //   // imageContainer.style.transform = `translateX(${0}%)`
-  //   return
-  // }
-
-}
-
-const rightMove = () => {
-
-  let slider = document.querySelectorAll('.image-container')
-  let lastSection = slider[slider.length - 1]
-
-  imageContainer.style.marginLeft = '0%'
-  imageContainer.style.transition = 'all .5s ease'
-
-  setTimeout(() => {
-    imageContainer.style.transition = 'none'
-    imageContainer.insertAdjacentElement('afterbegin', lastSection)
-    imageContainer.style.marginLeft = '-100%'
-  }, 500);
-
-  // let step
-  // slidesContainer.forEach((elem, index) => {
-  //   if (elem.classList.contains('active-slide')) {
-  //     elem.classList.remove('active-slide')
-  //     return step = index
-  //   }
-  // })
-  // let img = imageStep * -step
-  // // if (!slidesContainer[step--]) return console.log('hello')
-  // slidesContainer[--step].classList.add('active-slide')
-  // imageContainer.style.transform = `translateX(${img + imageStep}%)`
-}
-
-const activeCenter = () => {
-  slidesContainer.forEach((elem, index) => {
-    if (elem.classList.contains('active-slide')) {
-      index > slidesContainer.length / 2
-        ?
-        leftMove()
-        :
-        rightMove()
-    }
-  })
-}
+// const activeCenter = () => {
+//   mySlides.forEach((elem, index) => {
+//     if (elem.classList.contains('active-slide')) {
+//       index > mySlides.length / 2
+//         ?
+//         leftMove()
+//         :
+//         rightMove()
+//     }
+//   })
+// }
 // activeCenter()
 // document.addEventListener('click', activeCenter)
 
-leftButton.addEventListener('click', leftMove)
-rightButton.addEventListener('click', rightMove)
-dotsContainer.addEventListener('click', activateSlide)
 
 // setInterval(() => {
-//   rightMove()
-// }, 5000);
+  //   rightMove()
+  // }, 5000);
